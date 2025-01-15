5317484c61306157f0181d4c03e98e2eedb2c4f6
wyli-crimexperience <wlolivete.crimexperience@uc-bcf.edu.ph>
2025-01-15 18:17:23 +0800

    gaze interaction

--- Assets/DevFiles/Wyli/Scripts/Wristwatch.cs
+++ Assets/DevFiles/Wyli/Scripts/Wristwatch.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 
 public class Wristwatch : MonoBehaviour {
+
     public void ActivateWristwatch(bool isActive) {
         ManagerGlobal.Instance.ActivateWristwatch(isActive);
     }
