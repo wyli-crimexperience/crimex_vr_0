06cb83fe7ae4b6dd0cb10d60a4119ca54e507be9
WyliOlivete <azeptoblade@gmail.com>
2025-02-12 15:53:48 +0800

    briefcase checkpoint

--- Assets/DevFiles/Wyli/Scripts/FingerprintPowder.cs
+++ Assets/DevFiles/Wyli/Scripts/FingerprintPowder.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public enum TypeFingerprintPowder {
     None,
-    VictoriaPureBlue,
-    Magnetic,
+    Black,
+    Fluorescent,
+    Gray,
     White,
-    Black
+    Magnetic
 }
 public class FingerprintPowder : MonoBehaviour {
 
     public TypeFingerprintPowder TypeFingerprintPowder;
 
-    [SerializeField] private MeshRenderer meshRenderer;
+    //[SerializeField] private MeshRenderer meshRenderer;
 
     private void Start() {
-        for (int i = 0; i < meshRenderer.materials.Length; i++) {
-            meshRenderer.materials[i] = new Material(meshRenderer.materials[i]);
-            meshRenderer.materials[i].color = ManagerGlobal.Instance.HolderData.GetColorOfFingerprintPowderType(TypeFingerprintPowder);
-        }
+        //for (int i = 0; i < meshRenderer.materials.Length; i++) {
+        //    meshRenderer.materials[i] = new Material(meshRenderer.materials[i]);
+        //    meshRenderer.materials[i].color = ManagerGlobal.Instance.HolderData.GetColorOfFingerprintPowderType(TypeFingerprintPowder);
+        //}
     }
 
 }
--- Assets/DevFiles/Wyli/Scripts/HolderData.cs
+++ Assets/DevFiles/Wyli/Scripts/HolderData.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "HolderData", menuName = "Scriptable Objects/HolderData")]
 public class HolderData : ScriptableObject {
 
-    [SerializeField] private Color colVictoriaPureBlue, colMagnetic, colWhite, colBlack;
+    [SerializeField] private Color colBlack, colFluorescent, colGray, colWhite, colMagnetic;
 
 
 
     public Color GetColorOfFingerprintPowderType(TypeFingerprintPowder typeFingerprintPowder) {
         switch (typeFingerprintPowder) {
-            case TypeFingerprintPowder.VictoriaPureBlue: { return colVictoriaPureBlue; }
-            case TypeFingerprintPowder.Magnetic: { return colMagnetic; }
-            case TypeFingerprintPowder.White: { return colWhite; }
             case TypeFingerprintPowder.Black: { return colBlack; }
+            case TypeFingerprintPowder.Fluorescent: { return colFluorescent; }
+            case TypeFingerprintPowder.Gray: { return colGray; }
+            case TypeFingerprintPowder.White: { return colWhite; }
+            case TypeFingerprintPowder.Magnetic: { return colMagnetic; }
 
             default: { return Color.white; }
         }
