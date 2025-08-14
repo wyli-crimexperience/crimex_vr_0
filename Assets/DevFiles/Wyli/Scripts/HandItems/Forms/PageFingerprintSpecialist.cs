724df4fe84aa8b39067db798b91b2c20c7b3c86d
WyliOlivete <azeptoblade@gmail.com>
2025-08-15 01:10:28 +0800

    fill-up fingerprint specialist form

--- Assets/DevFiles/Wyli/Scripts/HandItems/Forms/PageFingerprintSpecialist.cs
+++ Assets/DevFiles/Wyli/Scripts/HandItems/Forms/PageFingerprintSpecialist.cs
@@ -1,21 +1,29 @@
-using UnityEngine;
-
+using System;
 using TMPro;
+using UnityEngine;
+using static Unity.Tutorials.Core.Editor.RichTextParser;
 
 
 
 public class PageFingerprintSpecialist : Page {
 
-    [SerializeField] private TextMeshProUGUI txtDateTimeFilledUp;
+    [SerializeField] private TextMeshProUGUI txtCaseNumber, txtNatureOfCase, txtDate, txtWeatherCondition, txtNameOfVictim, txtTimeOfArrival, txtTimeDatePlaceOfOccurrence, txtLocationOfFingerprint;
     [SerializeField] private GameObject goSketch;
 
-    private bool hasWrittenDateTimeFilledUp, hasWrittenSketch;
+    private bool hasWrittenCaseNumber, hasWrittenNatureOfCase, hasWrittenDate, hasWrittenWeatherCondition, hasWrittenNameOfVictim, hasWrittenTimeOfArrival, hasWrittenTimeDatePlaceOfOccurrence, hasWrittenLocationOfFingerprint, hasWrittenSketch;
 
 
 
     private void Awake() {
-        txtDateTimeFilledUp.text = "";
+        txtCaseNumber.text = "";
+        txtNatureOfCase.text = "";
+        txtDate.text = "";
+        txtWeatherCondition.text = "";
+        txtNameOfVictim.text = "";
+        txtTimeOfArrival.text = "";
+        txtTimeDatePlaceOfOccurrence.text = "";
         goSketch.SetActive(false);
+        txtLocationOfFingerprint.text = "";
     }
 
 
@@ -25,14 +33,50 @@ public class PageFingerprintSpecialist : Page {
 
 
         while (true) {
-            if (!hasWrittenDateTimeFilledUp/* && hasCheckedTime*/) {
-                txtDateTimeFilledUp.text = $"{StaticUtils.DateTimeNowInEvening(ManagerGlobal.Instance.DateTimeIncident):MMM dd, yyyy}";
-                hasWrittenDateTimeFilledUp = true;
+            if (!hasWrittenCaseNumber) {
+                DateTime dateTimeNow = StaticUtils.DateTimeNowInEvening(ManagerGlobal.Instance.DateTimeIncident);
+                txtCaseNumber.text = $"CXP-{dateTimeNow:MM}-{dateTimeNow:yyyy}";
+                hasWrittenCaseNumber = true;
+                break;
+            }
+            if (!hasWrittenNatureOfCase) {
+                txtNatureOfCase.text = "Alleged Homicide Stabbing Incident";
+                hasWrittenNatureOfCase = true;
+                break;
+            }
+            if (!hasWrittenDate) {
+                txtDate.text = $"{StaticUtils.DateTimeNowInEvening(ManagerGlobal.Instance.DateTimeIncident):MMM dd, yyyy}";
+                hasWrittenDate = true;
+                break;
+            }
+            if (!hasWrittenWeatherCondition) {
+                txtWeatherCondition.text = "Fair";
+                hasWrittenWeatherCondition = true;
+                break;
+            }
+            if (!hasWrittenNameOfVictim) {
+                txtNameOfVictim.text = "Jose Martinez";
+                hasWrittenNameOfVictim = true;
+                break;
+            }
+            if (!hasWrittenTimeOfArrival) {
+                txtTimeOfArrival.text = $"{StaticUtils.DateTimeNowInEvening(ManagerGlobal.Instance.DateTimeInvestigatorArrived):MMM dd, yyyy}";
+                hasWrittenTimeOfArrival = true;
+                break;
+            }
+            if (!hasWrittenTimeDatePlaceOfOccurrence) {
+                txtTimeDatePlaceOfOccurrence.text = "#132 Legarda Rd., Baguio City";
+                hasWrittenTimeDatePlaceOfOccurrence = true;
                 break;
             }
             if (!hasWrittenSketch) {
                 goSketch.SetActive(true);
                 hasWrittenSketch = true;
+                break;
+            }
+            if (!hasWrittenLocationOfFingerprint) {
+                txtLocationOfFingerprint.text = "On the handle of the knife beside the victim";
+                hasWrittenLocationOfFingerprint = true;
                 break;
             }
             break;
